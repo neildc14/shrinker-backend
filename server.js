491844f43d8c69d0c17166740ba5b404d5188b4c
neil dela cruz <genoznightmare@gmail.com>
2022-12-17 07:52:50 +0800

    modified API endpoint

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //routes
-app.use("/sh", shortenLinks);
+app.use(shortenLinks);
 
 mongoose.set("strictQuery", true);
 mongoose.connect(process.env.MONGODB_URI, {
